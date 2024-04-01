6da11862f467948e39e9c60b563f78466596c0d3
JustBecerra <justj.becerra@gmail.com>
2024-04-01 15:32:16 -0300

    add correct logic to get questions

--- router/router.go
+++ router/router.go
@@ -2,11 +2,30 @@ package router
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+type QuestionText struct {
+	Text string `json:"text"`
+}
+
+type Questions struct {
+	Category string `json:"category"`
+	Id string `json:"id"`
+	Tags []string `json:"tags"`
+	Difficulty string `json:"difficulty"`
+	Regions []string `json:"regions"`
+	IsNiche bool `json:"isNiche"`
+	Question QuestionText `json:"question"`
+	CorrectAnswer string `json:"correctAnswer"`
+	IncorrectAnswers []string `json:"incorrectAnswers"`
+	Type string `json:"type"` 
+} 
 
 func getQuestions(c *gin.Context){
 	resp, err := http.Get("https://the-trivia-api.com/v2/questions")
@@ -20,15 +39,21 @@ func getQuestions(c *gin.Context){
 		c.JSON(resp.StatusCode, gin.H{"error": "Failed to fetch questions"})
 		return
 	}
-	
-	var result interface{} // Change the type to match the expected response
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+
+	responseData, err := io.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Print(err)
+    }
+
+	var result []Questions 
+	 
+	err = json.Unmarshal(responseData, &result)
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse response"})
 		return
 	}
 	
-	// Return the result to the client
-	c.JSON(http.StatusOK, result)
+	c.IndentedJSON(http.StatusOK, result)
 }
 
 func InitRouter() *gin.Engine {
