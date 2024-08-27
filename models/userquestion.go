65367d3b160939e16d15fa6083be7ad35b816049
JustBecerra <justj.becerra@gmail.com>
2024-08-26 22:36:39 -0300

    change property name

--- models/userquestion.go
+++ models/userquestion.go
@@ -3,6 +3,6 @@ package models
 type UserQuestion struct {
 	ID               int      `json:"id" gorm:"primaryKey"`
 	Question         string   `json:"question"`
-	CorrectAnswer    string   `json:"correctanswer"`
-	IncorrectAnswers []string `json:"incorrectanswers"`
+	CorrectAnswer    string   `json:"correctAnswer"`
+	IncorrectAnswers []string `json:"incorrectAnswers"`
 }
