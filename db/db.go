84a4850aa1f29db8d49684f33521827b58f7aecb
JustBecerra <justj.becerra@gmail.com>
2024-08-30 13:44:06 -0300

    add db railway connection

--- db/db.go
+++ db/db.go
@@ -4,19 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+
+	_ "github.com/lib/pq" // Import the PostgreSQL driver
 )
 
+// ConnectDB establishes a connection to the database using the connection URL
 func ConnectDB() (*sql.DB, error) {
-	// Get values from environment variables
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-
-	// Construct connection string
-	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbName)
+	// Get the connection URL from the environment variable
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
+	}
 
 	// Connect to the database
 	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	// Verify the connection is successful
+	err = db.Ping()
 	if err != nil {
 		return nil, err
 	}
