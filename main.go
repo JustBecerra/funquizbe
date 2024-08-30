15edc37d33807d55b603a8f0874fb7e312dabeb6
JustBecerra <justj.becerra@gmail.com>
2024-08-30 12:50:08 -0300

    solve naming issue

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
-package handler
+package main
 
 import (
 	router "funquizbe/routes"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // Handler is the exported function Vercel will use as the entry point
-func Handler(w http.ResponseWriter, r *http.Request) {
-	// Initialize the Gin router
-	router := router.InitRouter()
-
-	// Set Gin to release mode
+func main(){
 	gin.SetMode(gin.ReleaseMode)
-
-	// Serve the request using Gin
-	router.ServeHTTP(w, r)
-}
+	r := router.InitRouter()
+	r.Run(":8080")
+   }
